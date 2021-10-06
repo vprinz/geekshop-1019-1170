a0bfa22e6197e5e5bf9e757a90f99023821f1ff9
Vokler <valerypavlikov@yandex.ru>
2021-10-06 22:01:58 +0300

    lesson2

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from products.views import index, products
+from products.views import index, products, test_context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('test-context/', test_context, name='test_context'),
 ]
--- products/views.py
+++ products/views.py
@@ -9,3 +9,18 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'GeekShop',
+        'header': 'Добро пожловать на сайт!',
+        'user': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+            {'name': 'Синяя куртка The North Face', 'price': 23725},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+        ],
+        'is_promotion': True,
+    }
+    return render(request, 'products/test_context.html', context)
