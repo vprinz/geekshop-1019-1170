80d380a60f92945fbd4584084594633b512a1847
Vokler <valerypavlikov@yandex.ru>
2021-10-11 21:57:38 +0300

    added MEDIA for project

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from products.views import index, products, test_context
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('products/', products, name='products'),
     path('test-context/', test_context, name='test_context'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
