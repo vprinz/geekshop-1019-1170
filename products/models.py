d0955ce4c6ed2ae895733529c6f906e5cc5ce1f5
Vokler <valerypavlikov@yandex.ru>
2021-10-11 21:31:51 +0300

    added __str__ methods for models

--- products/models.py
+++ products/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 
-# модели = таблицы в БД
-
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -15,3 +16,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
