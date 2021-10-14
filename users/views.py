a9d6cb8734aadab678adb48ff484ea6673f40983
Vokler <valerypavlikov@yandex.ru>
2021-10-14 20:41:03 +0300

    added login and registration html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {'title': 'GeekShop - Авторизация'}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {'title': 'GeekShop - Регистрация'}
+    return render(request, 'users/registration.html', context)
