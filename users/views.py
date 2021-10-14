0a68cfa53fb4e73ef10d6055e9bfe6b50c5f8316
Vokler <valerypavlikov@yandex.ru>
2021-10-14 20:59:28 +0300

    added userLoginForm

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
+
 
 def login(request):
-    context = {'title': 'GeekShop - Авторизация'}
+    form = UserLoginForm()
+    context = {
+        'title': 'GeekShop - Авторизация',
+        'form': form,
+    }
     return render(request, 'users/login.html', context)
 
 
