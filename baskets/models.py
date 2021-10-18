dcc29e709295dfafc3acb3086bb40ca7085dae75
Vokler <valerypavlikov@yandex.ru>
2021-10-18 22:05:29 +0300

    added sum() method for Basket

--- baskets/models.py
+++ baskets/models.py
@@ -12,3 +12,6 @@ class Basket(models.Model):
 
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
